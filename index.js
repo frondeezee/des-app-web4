74b8d7bdd346376408cbd62fb37014f808690bac
Guillermo Lerma Vera <090280@mail.unacar.mx>
2022-03-17 09:40:08 -0600

    1

--- index.js
+++ index.js
@@ -1,94 +1,96 @@
 
 
-let canciones = {
+
 const C1={
     nombre: 'Walking With You',
     artista:'Ivan Mateluna',
     álbum: 'The Annual Compilation 2010',
-    duración: '4.08'
+    duración: 4.08
 }
+
 const C2={
     nombre: 'Lonely Girl',
     artista: 'Oceanlab',
     álbum: 'The Annual Compilation 2010',
-    duración: '3.56'
+    duración: 3.56
 }
 const C3={
     nombre: 'We Belong',
     artista: 'Ferry Corsten',
     álbum: 'The Annual Compilation 2010',
-    duración: '2.55'
+    duración: 2.55
 }
 const C4={
     nombre: 'Coma Aid',
     artista: 'Marco V',
     álbum: 'The Annual Compilation 2010',
-    duración: '4.02'
+    duración: 4.02
 }
 const C5={
     nombre: 'Don‚t Wake Me Up',
     artista: 'Dj Shah Feat. Inger Hansen',
     álbum: 'The Annual Compilation 2010',
-    duración: '3.53'
+    duración: 3.53
 }
 const C6={
     nombre: 'Never Say Never',
     artista: 'Armin Van Buuren Feat. Jacqueline Govaert',
     álbum: 'The Annual Compilation 2010',
-    duración: '3.36'
+    duración: 3.36
 }
 const C7={
     nombre: 'Change Your Mind',
     artista: 'Sunlounger Feat. Kyler England',
     álbum: 'The Annual Compilation 2010',
-    duración: '4.05'
+    duración: 4.05
 }
 const C8={
     nombre: 'Man On The Run',
     artista: 'Dash Berlin With Cerf, Mitiska & Jaren',
     álbum: 'The Annual Compilation 2010',
-    duración: '3.25'
+    duración: 3.25
 }
 const C9={
     nombre: 'Lost',
     artista: 'Sunlounger Feat. Zara',
     álbum: 'The Annual Compilation 2010',
-    duración: '3.51'
+    duración: 3.51
 }
 const C10={
     nombre: 'L.A. Nights',
     artista: 'Atb',
     álbum: 'The Annual Compilation 2010',
-    duración: '4.17'
+    duración: 4.17
 }
 const C11={
     nombre: 'Made Of Love',
     artista: 'Ferry Corsten Feat. Betsy Larkin',
     álbum: 'The Annual Compilation 2010',
-    duración: '3.47'
+    duración: 3.47
 }
 const C12={
     nombre: 'Find Yourself',
     artista: 'John O‚callaghan Feat. Sarah Howells',
     álbum: 'The Annual Compilation 2010',
-    duración: '3.33'
+    duración: 3.33
 }
 const C13={
     nombre: 'To The Sky',
     artista: 'Roger Shah Feat. Chris Jones',
     álbum: 'The Annual Compilation 2010',
-    duración: '3.33'
+    duración: 3.33
 }
 const C14={
     nombre: 'The New World'
     artista: 'Markus Schulz',
     álbum: 'The Annual Compilation 2010',
-    duración: '3.19'
+    duración: 3.19
 }
 const C15={
     nombre: 'In And Out Of Love',
     artista: 'Armin Van Buuren Feat. Sharon Den Adel',
     álbum: 'The Annual Compilation 2010',
-    duración: '4.55'
+    duración: 4.55
 }
-}
+
+console.log(C1);
