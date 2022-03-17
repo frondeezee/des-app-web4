52a469ac63b99482a52459b6be6e5681f404da6c
Guillermo Lerma Vera <090280@mail.unacar.mx>
2022-03-17 15:26:28 -0600

    3

--- scripts/index.js
+++ scripts/index.js
@@ -5,92 +5,107 @@ const canciones=[
     nombre: 'Walking With You',
     artista:'Ivan Mateluna',
     álbum: 'The Annual Compilation 2010',
-    duración: 4.08
+    duración: 4.08,
+    image: 'images/The Annual Compilation 2010.jpg'
 },
 
     C2={
     nombre: 'Lonely Girl',
     artista: 'Oceanlab',
     álbum: 'The Annual Compilation 2010',
-    duración: 3.56
+    duración: 3.56,
+    image: 'images/The Annual Compilation 2010.jpg'
 },
     C3={
     nombre: 'We Belong',
     artista: 'Ferry Corsten',
     álbum: 'The Annual Compilation 2010',
-    duración: 2.55
+    duración: 2.55,
+    image: 'images/The Annual Compilation 2010.jpg'
 },
     C4={
     nombre: 'Coma Aid',
     artista: 'Marco V',
     álbum: 'The Annual Compilation 2010',
-    duración: 4.02
+    duración: 4.02,
+    image: 'images/The Annual Compilation 2010.jpg'
 },
     C5={
     nombre: 'Dont Wake Me Up',
     artista: 'Dj Shah Feat. Inger Hansen',
     álbum: 'The Annual Compilation 2010',
-    duración: 3.53
+    duración: 3.53,
+    image: 'images/The Annual Compilation 2010.jpg'
 },
     C6={
     nombre: 'Never Say Never',
     artista: 'Armin Van Buuren Feat. Jacqueline Govaert',
     álbum: 'The Annual Compilation 2010',
-    duración: 3.36
+    duración: 3.36,
+    image: 'images/The Annual Compilation 2010.jpg'
 },
     C7={
     nombre: 'Change Your Mind',
     artista: 'Sunlounger Feat. Kyler England',
     álbum: 'The Annual Compilation 2010',
-    duración: 4.05
+    duración: 4.05,
+    image: 'images/The Annual Compilation 2010.jpg'
 },
     C8={
     nombre: 'Man On The Run',
     artista: 'Dash Berlin With Cerf, Mitiska & Jaren',
     álbum: 'The Annual Compilation 2010',
-    duración: 3.25
+    duración: 3.25,
+    image: 'images/The Annual Compilation 2010.jpg'
 },
     C9={
     nombre: 'Lost',
     artista: 'Sunlounger Feat. Zara',
     álbum: 'The Annual Compilation 2010',
-    duración: 3.51
+    duración: 3.51,
+    image: 'images/The Annual Compilation 2010.jpg'
 },
     C10={
     nombre: 'L.A. Nights',
     artista: 'Atb',
     álbum: 'The Annual Compilation 2010',
-    duración: 4.17
+    duración: 4.17,
+    image: 'images/The Annual Compilation 2010.jpg'
 },
     C11={
     nombre: 'Made Of Love',
     artista: 'Ferry Corsten Feat. Betsy Larkin',
     álbum: 'The Annual Compilation 2010',
-    duración: 3.47
+    duración: 3.47,
+    image: 'images/The Annual Compilation 2010.jpg'
 },
     C12={
     nombre: 'Find Yourself',
     artista: 'John OCallaghan Feat. Sarah Howells',
     álbum: 'The Annual Compilation 2010',
-    duración: 3.33
+    duración: 3.33,
+    image: 'images/The Annual Compilation 2010.jpg'
 },
     C13={
     nombre: 'To The Sky',
     artista: 'Roger Shah Feat. Chris Jones',
     álbum: 'The Annual Compilation 2010',
-    duración: 3.33
+    duración: 3.33,
+    image: 'images/The Annual Compilation 2010.jpg'
 },
     C14={
     nombre: 'The New World',
     artista: 'Markus Schulz',
     álbum: 'The Annual Compilation 2010',
-    duración: 3.19
+    duración: 3.19,
+    image: 'images/The Annual Compilation 2010.jpg'
 },
     C15={
     nombre: 'In And Out Of Love',
     artista: 'Armin Van Buuren Feat. Sharon Den Adel',
     álbum: 'The Annual Compilation 2010',
-    duración: 4.55
+    duración: 4.55,
+    image: 'images/The Annual Compilation 2010.jpg'
 }
 ]
 console.log(C1)
